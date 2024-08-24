584b242ce0f437c9b91b181f7ee7b20c5932c8a3
Дмитрий <dima.voron13666@gmail.com>
2024-08-24 19:01:11 +0300

    111

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            ParametersServiceClass.validateParameters("admin", "34664","34664");
+            ParametersServiceClass.validateParameters("admin", "34664", "34664");
         } catch (WrongLoginException e) {
             System.out.println("Неверно введен логин");
         } catch (WrongPasswordException e) {
